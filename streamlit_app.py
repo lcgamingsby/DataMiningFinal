799cdeee4f4eb0e8857fe285f15f4936c4bce53f
Michael Hartanto <124630131+lcgamingsby@users.noreply.github.com>
2025-01-21 08:49:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import requests
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score
 from datetime import datetime, timedelta
 import streamlit as st
